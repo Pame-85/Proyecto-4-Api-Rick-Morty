435a514f19c3efdc4512d0e5544906f7738ff78d
Maria Rey <maria.x.rey.-nd@disney.com>
2022-03-03 20:52:02 -0300

    observaciones

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 //Busqueda
+
+// Deja un espacio antes y despues de los =, asi:
+// const busquedaPersonaje = document.querySelector(".personaje-button")
+// let paginaPrincipal = 1
+
+// nunca usas esta variable
 const busquedaPersonaje=document.querySelector(".personaje-button")
+// nunca usas esta variable
 const busquedaForm=document.querySelector("#form-busqueda")
 const containerCards=document.querySelector(".cards")
 //Paginacion
@@ -7,8 +14,11 @@ const btnPrev=document.querySelector("#prev-button")
 const btnNext=document.querySelector("#next-button")
 
 //Modal Detalle
+// nunca usas esta variable
 const btnCloseModal=document.querySelector(".close-button")
+// nunca usas esta variable
 const modalDetallePcipal=document.querySelector("#modal-detalle")
+// nunca usas esta variable
 const containerModal=document.querySelector(".card-modal-detalle")
 
 
@@ -19,6 +29,7 @@ let ultimaPagina=0
 
 
 const buscarPersonaje = () => {
+    // no dejes console log en una entrega
     console.log(paginaPrincipal)
     fetch(`https://rickandmortyapi.com/api/character?page=${paginaPrincipal}`)
         .then((res) => {
@@ -27,10 +38,14 @@ const buscarPersonaje = () => {
         .then((data) => {
             ultimaPagina=(data.info.pages)
             hacerHtmlCards(data.results)
+            // no dejes codigo comentado en una enetrega
             //personajePrincipal()
 
         })
 }
+
+// todas las funciones que se ejecutan al principio, es mejor que esten al final de todo, 
+// todas juntas. asi el flujo de ejecucion del codigo es mas facil de entender
 buscarPersonaje()
 
 
@@ -67,6 +82,7 @@ const buscarPersonajeFiltrado = (name) => {
         })
         .then((data) => {
             cardPrincipal(data.results)
+            // no dejes codigo comentado
             //personajePrincipal()
 
 
@@ -88,6 +104,13 @@ const cardPrincipal = (character) => {
 }
 //Paginacion
 
+// Estos botones estan asociados a buscarPersonaje, no a buscarPersonajeFiltrado. 
+// Lo que ocurre es que cuando busco un personaje, por ejemplo, Rick, y luego voy a la pagina 2, 
+// veo los resiltados de todos los personajes, no de rick solamente
+
+// Tenes que, o bien hacer diferentes botones para la busqueda, o hacer una funcion que sirva para las dos cosas
+// Con la api de rick y morty es facil hacer lo segundo, agrega name=${inputNamePersonaje.value} al fetch de 
+// buscarPersonaje y deberia andar
 btnNext.onclick = () => {
     paginaPrincipal++
     if(paginaPrincipal===ultimaPagina){
---
+++ observaciones.md
@@ -0,0 +1,10 @@
+
+Pame, 
+
+Felicitaciones por la entrega de tu trabajo! Como siempre, te dejo varios comentarios a lo largo de tus archivos. A nivel contenidos, creo que tu TP demuestra la comprensión de los temas vistos. 
+
+De tu web en sí estoy satisfecha, pero me gustaría que siguieras trabajando en ella, ya que podrías aprender mucho sobre lo ya hecho. En particular, sería importantisimo que la seccion de detalles, como para dejar completas las consignas y hacer la web 100% funcional, y aprender cómo hacerlo, ya que es la parte con mas dificultad. Tambien te dejo un comentario sobre el paginado para que puedas mejorarlo. 
+
+A tu Sass le falta algo de cariño tambien, seria mucho mejor si aprovecharas mejor el anidado. Eso tambien es un gran aprendizaje para el futuro, te aseguro que vale la pena estudiarlo.
+
+Nota final: 7
